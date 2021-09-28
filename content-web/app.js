b22151f5959a1593380942b0617dcbcfde153ce2
Oscar Hallgren <oscar.hallgren@hoglandet.se>
2021-09-28 12:33:52 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,11 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("[YOUR APPINSIGHTS KEY]");
+appInsights.start();
+
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://52.142.0.80:3001";
 
